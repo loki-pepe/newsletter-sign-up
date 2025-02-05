f937fd0822e14c5a67f1c20af41191ea0dfcb8d5
Lovro <lovroperaic@gmail.com>
2025-02-05 17:08:02 +0100

    set toggle cards functionality

--- static/script.js
+++ static/script.js
@@ -1,5 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    let standardCard  = document.querySelector('.card#standard');
+    let successCard = document.querySelector('.card#success');
 
+    let dismissButton = successCard.querySelector('#dismiss')
+    dismissButton.addEventListener('click', () => toggleCards());
+
+    let subscribeForm = standardCard.querySelector('#subscribe');
+    subscribeForm.addEventListener('submit', e => handleSubmit(e));
+
+    let subscriber = successCard.querySelector('#subscribed-email');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        logEmail(e);
+        toggleCards();
+    }
+
+    function logEmail(e) {
+        subscriber.innerHTML = Object.fromEntries(new FormData(e.target)).email;
+    }
+
+    function toggleCards() {
+        standardCard.style.display === 'none' ? standardCard.style.display = 'flex' : standardCard.style.display = 'none';
+        successCard.style.display === 'flex' ? successCard.style.display = 'none' : successCard.style.display = 'flex';
+    }
     
 })
