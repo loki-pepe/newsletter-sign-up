4ad95a94dca0b7b9177c340ea01822cf08218cbf
Lovro <lovroperaic@gmail.com>
2025-02-05 17:27:50 +0100

    set up for email validation

--- static/script.js
+++ static/script.js
@@ -7,23 +7,47 @@ document.addEventListener('DOMContentLoaded', () => {
     dismissButton.addEventListener('click', () => toggleCards());
 
     let subscribeForm = standardCard.querySelector('#subscribe');
+    subscribeForm.addEventListener('input', () => hideError());
     subscribeForm.addEventListener('submit', e => handleSubmit(e));
 
     let subscriber = successCard.querySelector('#subscribed-email');
 
     function handleSubmit(e) {
         e.preventDefault();
-        logEmail(e);
-        toggleCards();
+        let email = getEmail(e);
+        if (validateEmail(email)) {
+            logEmail(email);
+            toggleCards();
+        } else {
+            showError();
+        }
     }
 
-    function logEmail(e) {
-        subscriber.innerHTML = Object.fromEntries(new FormData(e.target)).email;
+    function getEmail(e) {
+        return Object.fromEntries(new FormData(e.target)).email
+    }
+
+    function hideError() {
+        subscribeForm.classList.remove('invalid');
+    }
+
+    function logEmail(email) {
+        subscriber.innerHTML = email;
     }
 
     function toggleCards() {
         standardCard.style.display === 'none' ? standardCard.style.display = 'flex' : standardCard.style.display = 'none';
         successCard.style.display === 'flex' ? successCard.style.display = 'none' : successCard.style.display = 'flex';
     }
+
+    function showError() {
+        subscribeForm.classList.add('invalid');
+        subscribeForm.querySelector('button').blur();
+        subscribeForm.querySelector('#email').focus();
+    }
+
+    function validateEmail(email) {
+       return (email ? true : false);
+    }
     
-})
+});
